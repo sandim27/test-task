428256c32ed7a8d10d26da4914bf826fd83b5fc9
Dmitry Santocky <dzmitry.santotski@instinctools.ru>
2015-04-10 19:17:17 +0300

    Init commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,6 +76,7 @@ module.exports = function(grunt) {
         },
     });
 
+
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
