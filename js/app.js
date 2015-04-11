5f452a0b259221f9f319afd052eb8cc58a897d5c
= <=>
2015-04-11 09:03:04 +0300

    New changelist

--- js/app.js
+++ js/app.js
@@ -1,25 +1,12 @@
-var app = angular.module('app', ['ngRoute']).config(function($routeProvider){
-	$routeProvider.when('/groups', {
-		templateUrl: 'groups.html',
-		controller: 'LoginController'
-	});
-	$routeProvider.when('/home', {
-		templateUrl: 'home.html',
-		controller: 'HomeController'
-	});
-	$routeProvider.otherwise({redirectTo:'login'});
-});
+var app = angular.module('app', ['ngRoute'])
 
-app.controller('LoginController', function($location,$scope){ 
- $scope.login   = function(){
- 	if($scope.input.login ==='admin')
-		$location.path('home')
- }
-});
-app.controller('HomeController', function($location,$scope){
-	$scope.loginout   = function(){
- 	
-		$location.path('login')
-	}
- 
+app.controller('mainCtrl', function ($http, $scope) {
+    $http.get("./json/users.json")
+        .success(function (data) {
+            $scope.tableUsers = data;
+        });
+    $http.get("./json/groups.json")
+        .success(function (data) {
+            $scope.tableGroups = data;
+        });
 });
